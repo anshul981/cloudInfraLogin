ecd1edb211cbe3445a61d794c17ab708206c746a
anshul981 <guptaanshul981@gmail.com>
2020-11-17 16:30:28 +0530

    Adding necessary project files

---
+++ src/app/account/login.component.ts
@@ -0,0 +1,87 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AccountService, AlertService } from '@app/_services';
+
+declare var gapi: any;
+
+@Component({ templateUrl: 'login.component.html' })
+export class LoginComponent implements OnInit {
+    form: FormGroup;
+    loading = false;
+    submitted = false;
+    returnUrl: string;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private accountService: AccountService,
+        private alertService: AlertService
+    ) { }
+    
+    public btnRender(): void {
+        const options = {
+          scope: 'profile email', 
+          width: 250,
+          height: 40,
+          longtitle: true,
+          theme: 'dark',
+          onsuccess: (googleUser => {
+            let profile = googleUser.getBasicProfile();
+            console.log('Token || ' + googleUser.getAuthResponse().id_token);
+            console.log('ID: ' + profile.getId());
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail());
+         
+         // your-code-goes-here
+           
+          }),
+          onfailure: ((error) => {
+            console.log('failure', error);
+          })
+        };
+        gapi.signin2.render('googleBtn', options);
+      }
+
+    ngOnInit() {
+        this.btnRender();
+        this.form = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.form.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // reset alerts on submit
+        this.alertService.clear();
+
+        // stop here if form is invalid
+        if (this.form.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.accountService.login(this.f.username.value, this.f.password.value)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.router.navigate([this.returnUrl]);
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
+    }
+}
