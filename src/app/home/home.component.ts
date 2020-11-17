d76866b27f8f37836541571a6a3c5ab28a194f3c
anshul981 <68767199+anshul981@users.noreply.github.com>
2020-11-18 00:54:26 +0530

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
-﻿import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { User } from '@app/_models';
 import { AccountService } from '@app/_services';
+
+declare var gapi: any;
 
 @Component({ templateUrl: 'home.component.html' })
 export class HomeComponent {
@@ -10,4 +12,34 @@ export class HomeComponent {
     constructor(private accountService: AccountService) {
         this.user = this.accountService.userValue;
     }
+    public btnRender(): void {
+        const options = {
+          scope: 'profile email', 
+          width: 250,
+          height: 40,
+          longtitle: true,
+          theme: 'dark',
+          onsuccess: (googleUser => {
+            let profile = googleUser.getBasicProfile();
+            console.log('Token || ' + googleUser.getAuthResponse().id_token);
+            console.log('ID: ' + profile.getId());
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail());
+            var img = profile.getImageUrl();
+            document.getElementById('proflImage').innerHTML = '<img src="' +img+'"/>';
+            document.getElementById('userEmail').innerHTML = profile.getEmail();
+         // your-code-goes-here
+           
+          }),
+          onfailure: ((error) => {
+            console.log('failure', error);
+          })
+        };
+        gapi.signin2.render('googleBtn', options);
+        // this.returnUrl = '';
+      }
+      ngOnInit() {
+        this.btnRender();
+      }
 }
